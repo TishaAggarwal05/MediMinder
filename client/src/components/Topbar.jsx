f46d5c3299997906efe3f03e28522c952117ae6a
tisha <aggarwal.tisha05@gmail.com>
2025-07-20 07:57:08 +0530

    logo

--- client/src/components/Topbar.jsx
+++ client/src/components/Topbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Container, Row, Col, Card, Button, Tab, Nav } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
-import MediLogo from '../assets/MediLogo.png';
+import MLogo from '../assets/MLogo.png';
 export default function Topbar({ toggleSidebar }) {
   return (
     <Navbar className="bar-style" style={{ backgroundColor: '#1b403bff',width: '100%', zIndex: 1050 ,position:"fixed" }} variant="dark" expand="md">
@@ -20,7 +20,7 @@ export default function Topbar({ toggleSidebar }) {
         <Navbar.Brand href="/" className="d-flex align-items-center">
           <img
             alt=""
-            src={MediLogo}
+            src={MLogo}
             width="30"
             height="30"
             className="rounded-circle me-2"
